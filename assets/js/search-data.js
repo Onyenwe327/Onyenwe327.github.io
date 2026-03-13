761b7e30a01f4429dd930277318f7007b4956cd2
Onyenwe327 <61456951+Onyenwe327@users.noreply.github.com>
2026-03-13 01:31:58 +0000

    Deploying to gh-pages from @ Onyenwe327/onyenwe327.github.io@f95c8a68a0e5ee75ffdebc7fcb5136d88861672b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Some summaries of my work.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
